1110c24a15436714faaa6f41e35253add6d14084
leapar <9395462@qq.com>
2017-07-27 13:51:59 +0800

    edit

--- images/librenms/files/opt/librenms/config.docker.php
+++ images/librenms/files/opt/librenms/config.docker.php
@@ -9,7 +9,7 @@ $config['db']['extension'] = 'mysqli';
 
 $config['user'] = 'librenms';
 $config['base_url'] = getenv('BASE_URL');
-$config['snmp']['community'] = array("public");
+
 $config['snmp']['timeout'] = 0.2;
 $config['auth_mechanism'] = "mysql";
 $config['rrd_purge'] = 0;
@@ -41,23 +41,39 @@ $config['autoscan']['snmp']['transports'] = array(
     'udp',
     #'tcp',
 );
+
+
+
+
+$g_metric_data = [];
+$g_metric_service = [];
+$g_metric_check = [];
+
+
+
+$config['agent']['host'] = 'http://172.29.231.177:8000/snmp/v1/';
+
+$config['agent_host'] = 'snmp1';
+
+
+//以下内容需要配置
 # 默认扫描协议 大部分都是v2c 可以是 array('v2c', 'v3', 'v1') 如果不填那么会扫描三次 性能问题
 $config['autoscan']['snmp']['version'] = 'v2c';
 
+#默认的snmp 密码
+$config['snmp']['community'] = array("public");
+
+#需要发现的网段
 $config['nets'][] = "172.29.231.0/24";
 $config['nets'][] = "172.29.228.0/24";
 $config['nets'][] = "172.29.226.0/24";
 $config['nets'][] = "172.29.201.0/24";
 $config['nets'][] = "172.29.230.0/24";
 
-$g_metric_data = [];
-$g_metric_service = [];
-$g_metric_check = [];
-
+#特殊IP的密码
 $config['snmp']['v2c']['community']['172.29.201.1'] = 'h3c';
 $config['snmp']['v2c']['community']['172.29.226.1'] = 'Purvar123';
 $config['snmp']['v2c']['community']['172.29.230.46'] = 'Purvar123';
 
-$config['agent']['host'] = 'http://172.29.231.177:8000/snmp/v1/';
+#个人key
 $config['agent']['key'] = 'd345ee0759994410b47d098230c4a188';//'8571ffe00284475cb06b17847a97b6bf';
-$config['agent_host'] = 'snmp1';
